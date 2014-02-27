bede36bc2a3c2e68dbf1f0cbc7c542ac775c1d50
epicodus <student@epicodus.com>
2014-02-26 17:23:57 -0800

    added basic functionality to expense-tracker project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var Category = {
+  create: function(catName) {
+    var category = Object.create(Category);
+      category.initialize(catName);
+      return category;
+  },
+  initialize: function(catName) {
+    this.catName = catName;
+    this.purchaseArray = [];
+  }
+};
+
 var Purchase = {
   create: function(desc, amount) {
     if (desc !== "" && amount !== "") {
@@ -22,14 +34,47 @@ var convertToDollar = function(amount) {
   return parseFloat(amount);
 };
 
+
+
+
 $(document).ready(function() {
-  $("form#new-purchase").submit(function(event) {
+  
+  $("form#new-category").submit(function(event) {
     event.preventDefault();
 
-    var newPurchase = Purchase.create($("input#new-purchase-desc").val(), 
-                                      $("input#new-purchase-amount").val());
-    $("tbody#purchases").append("<tr><td>" + newPurchase.desc + "</td>" + 
-                             "<td class='money'>$" + convertToDollar(newPurchase.amount).toFixed(2) + "</td></tr>");
- this.reset();    
- });
+    var newCategory = Category.create($("input#new-category-name").val());
+    console.log(newCategory);
+    $("ul#categories").append("<li><span class='category'>" + newCategory.catName + "<span></li>");
+
+    $(".category").last().click(function() {
+      $("#show-purchase").show();
+      $(".add-title").text("Add purchase to " + newCategory.catName + ":");
+      $(".title").text(newCategory.catName + ":");
+
+      $("td.items").remove("");
+      $("td.money").remove("");
+
+      newCategory.purchaseArray.forEach(function(purchase) {
+        $("tbody#purchases").append("<tr><td class='items'>" + newPurchase.desc + "</td>" + 
+                                    "<td class='money'>$" + convertToDollar(newPurchase.amount).toFixed(2) + 
+                                    "</td></tr>");
+      });
+      $("form#new-purchase").submit(function(event) {
+        event.preventDefault();
+
+        var newPurchase = Purchase.create($("input#new-purchase-desc").val(), 
+                                          $("input#new-purchase-amount").val());
+        newCategory.purchaseArray.push(newPurchase);
+        console.log(newPurchase);
+        console.log(newCategory);
+        console.log(newPurchase.desc);
+        $("tbody#purchases").append("<tr><td class='items'>" + newPurchase.desc + "</td>" + 
+                                    "<td class='money'>$" + convertToDollar(newPurchase.amount).toFixed(2) + 
+                                    "</td></tr>");
+        
+        this.reset();    
+      });
+    });
+  this.reset();
+  });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,24 @@
+describe("Category", function() {
+  describe("create", function() {
+    it("creates an instance of a category object", function() {
+      var testCategory = Category.create();
+      Category.isPrototypeOf(testCategory).should.equal(true);
+    });
+  });
+  describe("initialize", function() {
+    it("sets name property", function() {
+      var testCategory = Object.create(Category);
+      testCategory.initialize("Pizza");
+      testCategory.catName.should.equal("Pizza");
+    });
+    it("sets up an empty array for the purchase propery", function() {
+      var testCategory = Object.create(Category);
+      testCategory.initialize("Pizza");
+      testCategory.purchaseArray.should.eql([]);
+    });
+  });
+});
+
 describe("Purchase", function() {
   describe("create", function() {
     it("creates an instance of a Purchase object", function() {
